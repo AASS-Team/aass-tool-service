fb3ab6b934fb8c257f50b83c125a6c1419a1d202
František Gič <mcfreddie777@gmail.com>
2022-05-02 23:51:46 +0200

    Implement network communication between containers

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = require_env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = require_env("DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
